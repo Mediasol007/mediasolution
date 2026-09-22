549acb14e7db9d2d5690c4b5baa513aa598ace73
root <root@vmi2968062.contaboserver.net>
2026-09-22 12:17:47 +0200

    Step 2: Sidebar Routing + Create Client

--- server.js
+++ server.js
@@ -1,47 +1,9 @@
 const express=require("express");
 const path=require("path");
-const sqlite3=require("sqlite3").verbose();
-
 const app=express();
-const db=new sqlite3.Database("./data/msbp.db");
+const PORT=3000;
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views/index.html")));
 
-db.serialize(()=>{
-db.run(`CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY,
-username TEXT UNIQUE,
-password TEXT
-)`);
-
-db.get("SELECT * FROM users WHERE username='admin'",(e,row)=>{
-if(!row){
-db.run("INSERT INTO users(username,password) VALUES('admin','admin123')");
-}
-});
-});
-
-app.get("/",(req,res)=>{
-res.sendFile(path.join(__dirname,"views","login.html"));
-});
-
-app.post("/login",(req,res)=>{
-const {username,password}=req.body;
-
-db.get(
-"SELECT * FROM users WHERE username=? AND password=?",
-[username,password],
-(err,row)=>{
-if(row){
-res.sendFile(path.join(__dirname,"views","index.html"));
-}else{
-res.send("<h2>Invalid Login</h2><a href='/'>Try Again</a>");
-}
-});
-});
-
-app.listen(3000,()=>{
-console.log("MSBP Running :3000");
-});
+app.listen(PORT,"0.0.0.0",()=>console.log("MSBP Running :3000"));
