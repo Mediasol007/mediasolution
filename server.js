228086d481ceb62f37945e7ec8c2e4b19a8c3628
root <root@vmi2968062.contaboserver.net>
2026-09-22 12:06:05 +0200

    Initial project structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const path=require("path");
+
+const app=express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname,"views","index.html"));
+});
+
+const PORT=3000;
+app.listen(PORT,()=>console.log("MSBP Running on "+PORT));
